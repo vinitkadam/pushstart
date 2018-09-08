2a2d306b95e009482cfbe921c34201f9f6fb023e
unknown <vinitkadam19987@gmail.com>
2018-09-08 21:50:53 +0530

    New onBoarding added

--- src/containers/OnBoarding/OnBoarding.js
+++ src/containers/OnBoarding/OnBoarding.js
@@ -1,127 +1,26 @@
-// import React, { Component } from 'react';
-// import { Text, View, Dimensions, Image, StatusBar } from 'react-native';
-// import { Icon, Button } from 'native-base';
-
-// import Swiper from './Swiper';
-// import { colors } from '../../colors';
-
-// const { width, height } = Dimensions.get('window');
-
-// class OnBoarding extends Component {
-
-//     render() {
-//         return (
-//             <View>
-//                 <StatusBar
-//                     backgroundColor="rgba(0,0,0,0.2)"
-//                     barStyle="light-content"
-//                 />
-//                 <View style={styles.container}>
-//                     <Swiper navigation={this.props.navigation}>
-//                         {/* First screen */}
-//                         <View style={[styles.slide]}>
-//                             <Image source={require('./images/one.png')} style={styles.imgStyle}/>
-//                             <Text style={styles.header}>
-//                                 Welcome to India’s largest community for Entrepreneur’s
-//                             </Text>
-//                         </View>
-//                         {/* Second screen */}
-//                         <View style={[styles.slide]}>
-//                             <Image source={require('./images/two.png')} />
-//                             <Text style={styles.header}>
-//                                 Learn, Share Knowledge and grow your business
-//                             </Text>
-//                         </View>
-//                         {/* Third screen */}
-//                         <View style={[styles.slide]}>
-//                             <Image source={require('./images/one.png')} />
-//                             <Text style={styles.header}>
-//                                 Build your personal brand and increase visibility
-//                             </Text>
-//                         </View>
-//                     </Swiper>
-//                 </View>
-//                 <View style={styles.bottonContainer}>
-//                     <Button info style={styles.buttonStyle} onPress={() => { this.props.navigation.navigate('login'); }}>
-//                         <Text style={styles.buttonTextStyle} >GET STARTED</Text>
-//                     </Button>
-//                 </View>
-//             </View>
-
-//         );
-//     }
-// }
-// const styles = {
-//     container: {
-//         backgroundColor: '#ffffff',
-//         height: height - 200,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
-//     header: {
-//         fontSize: 18,
-//         color: '#865ed0',
-//         marginHorizontal: 20,
-//         textAlign: 'center',
-//         marginBottom: 20,
-//         marginTop: 10,
-
-//     },
-//     slide: {
-//         flex: 1,
-//         justifyContent: 'space-between',
-//         paddingTop: 50,
-//         alignItems: 'center',
-//         padding: 15,
-//     },
-//     bottonContainer: {
-//         backgroundColor: 'white',
-//         height: 200,
-//         alignItems: 'center',
-//     },
-//     buttonStyle: {
-//         width: 150,
-//         padding: 5,
-//         alignSelf: 'center',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         backgroundColor: '#865ed0',
-//         borderRadius: 6,
-//         marginTop: 40,
-//     },
-//     buttonTextStyle: {
-//         color: 'rgba(255,255,255,1)',
-//         fontSize: 18,
-//     }
-// };
-
-// const iconStyles = {
-//     size: 100,
-//     color: "#FFFFFF"
-// };
-
-// export default OnBoarding;
-
-
-import { Image, View } from 'react-native';
+import { Image, View, Text, Dimensions, ImageBackground } from 'react-native';
 import React from 'react';
-import { Button } from 'react-native-elements';
 
-import Onboarding from 'react-native-onboarding-swiper';
+import { Button } from 'native-base';
+
+// import { Button } from 'react-native-elements';
+
+import Onboarding from 'react-native-onboarding-swiper'; // Version can be specified in package.json
 
 const Square = ({ isLight, selected }) => {
   let backgroundColor;
   if (isLight) {
-    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+    backgroundColor = selected ? 'rgba(134, 94, 208, 1)' : 'rgba(196, 177, 232, 1)';
   } else {
     backgroundColor = selected ? '#fff' : 'rgba(255, 255, 255, 0.5)';
   }
   return (
     <View
       style={{
-        width: 6,
-        height: 6,
-        marginHorizontal: 3,
+        width: 22,
+        height: 4,
+        marginHorizontal: 4,
+        borderRadius: 2,
         backgroundColor,
       }}
     />
@@ -133,79 +32,100 @@ const color = isLight => backgroundColor(!isLight);
 
 const Done = ({ isLight, ...props }) => (
   <Button
-    title={'Done'}
-    buttonStyle={{
-      backgroundColor: backgroundColor(isLight),
-    }}
-    containerViewStyle={{
-      marginVertical: 10,
+    style={{
+      backgroundColor: '#865ed0',
+      height: 70,
       width: 70,
-      backgroundColor: backgroundColor(isLight),
+      borderRadius: 35,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+      justifyContent: 'center'
     }}
-    textStyle={{ color: color(isLight) }}
-    {...props}
-  />
-);
-
-const Skip = ({ isLight, skipLabel, ...props }) => (
-  <Button
-    title={'Skip'}
-    buttonStyle={{
-      backgroundColor: backgroundColor(isLight),
-    }}
-    containerViewStyle={{
-      marginVertical: 10,
-      width: 70,
-    }}
-    textStyle={{ color: color(isLight) }}
     {...props}
   >
-    {skipLabel}
+    <Text style={{ color: 'white' }} >D</Text>
   </Button>
 );
 
 const Next = ({ isLight, ...props }) => (
   <Button
-    title={'Next'}
-    buttonStyle={{
-      backgroundColor: backgroundColor(isLight),
-    }}
-    containerViewStyle={{
-      marginVertical: 10,
+    style={{
+      backgroundColor: '#865ed0',
+      height: 70,
       width: 70,
-      backgroundColor: backgroundColor(isLight),
+      borderRadius: 35,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+      justifyContent: 'center',
+      shadowOffset: { height: 0, width: 0 },
+      elevation: 6,
+      shadowColor: '#865ed0',
+      shadowOpacity: 0.5,
+      shadowRadius: 6,
     }}
-    textStyle={{ color: color(isLight) }}
     {...props}
-  />
+  >
+    <Text style={{ color: 'white' }}>N</Text>
+  </Button>
 );
 
 const CustomButtons = () => (
   <Onboarding
+    showSkip={false}
+    bottomBarHeight={120}
+    bottomBarHighlight={false}
     DotComponent={Square}
     NextButtonComponent={Next}
-    SkipButtonComponent={Skip}
     DoneButtonComponent={Done}
     pages={[
       {
         backgroundColor: '#fff',
-        image: <Image source={require('./images/one.png')} />,
-        title: 'Onboarding',
-        subtitle: 'Done with React Native Onboarding Swiper',
+        image: <ImageBackground style={styles.img1} source={require('./images/one.png')} />,
+        title: 'Hello PushStarters',
+        subtitle: 'Welcome to India’s most active community for Entrepreneur’s',
       },
       {
-        backgroundColor: '#fe6e58',
-        image: <Image source={require('./images/two.png')} />,
-        title: 'The Title',
-        subtitle: 'This is the subtitle that sumplements the title.',
+        backgroundColor: '#fff',
+        image: <Image style={styles.img2} source={require('./images/two.png')} />,
+        title: 'Get Relevent Content ',
+        subtitle: 'Get notified about relevant content from Pushstart anytime, anywhere',
       },
       {
-        backgroundColor: '#999',
-        image: <Image source={require('./images/two.png')} />,
-        title: 'Triangle',
-        subtitle: "Beautiful, isn't it?",
+        backgroundColor: '#fff',
+        image: <Image style={styles.img3} source={require('./images/three.png')} />,
+        title: 'Archive Access',
+        subtitle: 'Access Relevent content from our Archive anytime, anywhere',
       },
     ]}
-  />
+/>
 );
-export default CustomButtons;
+
+const win = Dimensions.get('window');
+const ratio1 = win.width / 1079;
+const ratio2 = win.width / 1081;
+const ratio3 = win.width / 1082;
+
+const styles = {
+  img1: {
+    width: win.width+2,
+    height: 994 * ratio1,
+  },
+  img2: {
+    width: win.width+1,
+    height: 994 * ratio2,
+  },
+  img3: {
+    width: win.width+2,
+    height: 960 * ratio3,
+  }
+};
+
+export default class OnboardingScreen extends React.Component {
+
+  render() {
+    return (
+      <CustomButtons navigateToLogin={() => { this.props.navigation.navigate('login'); }} />
+    );
+  }
+}
+
